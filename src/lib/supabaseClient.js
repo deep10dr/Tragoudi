7670a509fc51303a9833461cfc20d05fa0bb5ab8
deep10dr <deepakofficial81@gmail.com>
2025-06-27 14:32:10 +0530

    added env file'

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://bxijsrxazpkfnbfonlrt.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ4aWpzcnhhenBrZm5iZm9ubHJ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU1NzU3MDAsImV4cCI6MjA2MTE1MTcwMH0.bLASp-wmOe-XqUTvF9FvUmawGgumRN-0TKLSEDqfTxI'; // Truncated
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY; // Truncated
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
